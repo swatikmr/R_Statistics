690ee1db31ff93c1dc7ebb01a31d99df791739c3
Siddharth <singh.sid930@gmail.com>
2019-05-20 17:28:02 -0400

    initial commit

---
+++ src/swati.m
@@ -0,0 +1,29 @@
+A = 1:10;
+P=diag(A);
+P=P.*P;
+
+for i = 1:10
+    sum_up = 0;
+    sum_down = 0;
+  for j = 1:10
+      if(j == i+1)
+          P(i,j) =1;
+      
+      elseif(j == i+2)
+          P(i,j) =1;
+          
+      elseif(j>i)
+          sum_up = P(i,j-1) + P(i,j-2);
+          P(i,j) = sum_up;               
+      end
+      
+      if(i == j+1)
+          P(i,j) =1;
+      end
+      
+      
+  end
+  
+end
+
+disp(P);
